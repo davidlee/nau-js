2e4617ed0a6db8f0d11171253c0eb632e4862087
David Lee <dav@davlee.com>
2023-08-24 18:37:21 +1000

    is this programming yet

--- test/test.ts
+++ test/test.ts
@@ -2,11 +2,7 @@ import test from 'node:test'
 import assert from 'node:assert/strict'
 //import assert from 'node:assert'
 
-
-
 test('synchronous passing test', (t) => {
   // This test passes because it does not throw an exception.
   assert.strictEqual(1, 1)
-})
-
 })
