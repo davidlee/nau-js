703ff8423e9a622700f52b32f706af62a5b0c1a7
David Lee <dav@davlee.com>
2023-08-31 00:28:51 +1000

    make it so the dispatcher can be configured for different adapters

--- src/dataLayerLoader.ts
+++ src/dataLayerLoader.ts
@@ -4,11 +4,37 @@ import * as H from './commandHandler'
 import * as R from './entryRepository'
 // import * as E from './entry'
 import * as F from './fileStoreAdapter'
+import * as M from './memoryStoreAdapter'
 
-export function dispatcher() {
-  const adapter = new F.FileStoreAdapter('/tmp/hello')
+import { DataStoreAdapter } from './dataStoreAdapter'
+
+interface Configuration {
+  adapter: string,
+  path:    string, // TODO fix typings
+}
+
+const defaults: Configuration  = {
+  adapter: 'file',
+  path: `${process.env.HOME}/.config/projector/entries.json`
+}
+export function dispatcher(options = {}) {
+  const props: Configuration = {...defaults, ...options}
+  let adapter: DataStoreAdapter
+  
+  switch(props.adapter) {
+    case 'file':
+      adapter = new F.FileStoreAdapter(props.path)
+      break
+    case 'memory':
+      adapter = new M.MemoryStoreAdapter()
+      break
+    default:
+      throw new Error('Illegal adapter')
+  }
+  
   const reader = new R.EntryReader(adapter)
   const writer = new R.EntryWriter(adapter)
+
   const handler = new H.CommandHandler(reader, writer)
 
   return new D.Dispatcher(handler)
